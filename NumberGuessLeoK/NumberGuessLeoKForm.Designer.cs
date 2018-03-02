d9551a4201922a0abca76b6ff88732e2e5b10d15
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-03-02 14:05:17 -0500

    Initial commit

---
+++ NumberGuessLeoK/NumberGuessLeoKForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace NumberGuessLeoK
+{
+    partial class frmNumberGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.picRightOrWrong = new System.Windows.Forms.PictureBox();
+            this.mnuMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRightOrWrong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 63);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(400, 24);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Please enter a number between 1 and 10:";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(581, 24);
+            this.mnuMenu.TabIndex = 1;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(431, 67);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(75, 155);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(91, 36);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // picRightOrWrong
+            // 
+            this.picRightOrWrong.Location = new System.Drawing.Point(271, 103);
+            this.picRightOrWrong.Name = "picRightOrWrong";
+            this.picRightOrWrong.Size = new System.Drawing.Size(260, 146);
+            this.picRightOrWrong.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picRightOrWrong.TabIndex = 4;
+            this.picRightOrWrong.TabStop = false;
+            // 
+            // frmNumberGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 261);
+            this.Controls.Add(this.picRightOrWrong);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblRequest);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmNumberGuess";
+            this.Text = "Number Guess by Leo Kay";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRightOrWrong)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.PictureBox picRightOrWrong;
+    }
+}
+
---
+++ NumberGuessLeoK/NumberGuessLeoKForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessLeoK
+{
+    public partial class frmNumberGuess : Form
+    {
+        public frmNumberGuess()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //local variables
+            const int NUM_SECRET = 9;
+            int numGuess;
+
+            //Get the number from the textbox
+            numGuess = int.Parse(txtInput.Text);
+
+            //If the user gets it right, display a checkmark and play a ding sound
+            //If the user gets it wrong, display a X and play a wrong sound
+
+            if (numGuess == NUM_SECRET)
+            {
+                this.picRightOrWrong.Image = Properties.Resources.checkmark;
+                System.Media.SoundPlayer dingSound = new System.Media.SoundPlayer(@"n:\StuShare\Semester 2 - Winter 2018\ICS3U Programming\Kay, Leo (s240457)\Unit 3 (Decisions)\NumberGuessLeoK\Correct_Answer_Sound_Effect.wav");
+                dingSound.Play();
+            }
+            else
+            {
+                this.picRightOrWrong.Image = Properties.Resources.red_x;
+                System.Media.SoundPlayer wrongSound = new System.Media.SoundPlayer(@"n:\StuShare\Semester 2 - Winter 2018\ICS3U Programming\Kay, Leo (s240457)\Unit 3 (Decisions)\NumberGuessLeoK\Wrong_Buzzer_Sound_Effect.wav");
+                wrongSound.Play();
+            }
+        }
+    }
+}
